d167657211acb64993c6ad2785d5c8852095572e
May Ng <mngmay@gmail.com>
2019-09-10 12:44:48 -0700

    Created GET for /api/cars

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use("api/cars", carsRouter);
+server.use("/api/cars", carsRouter);
 
 server.get("/", (req, res) => {
   res.send("<h3>API is connected and server is running</h3>");
--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -4,4 +4,14 @@ const db = require("../data/db-config.js");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  db("cars")
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to retriever cars." });
+    });
+});
+
 module.exports = router;
